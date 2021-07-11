968c24b6bdfb1c9110a89191f3648d230042bb89
apiiRR <ramadhanarafi437@gmail.com>
2021-07-11 22:09:34 +0700

    template stisla

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,95 @@
+            <div class="main-sidebar">
+                <aside id="sidebar-wrapper">
+                    <div class="sidebar-brand">
+                        <a href="index.html">Makmur Jaya</a>
+                    </div>
+                    <div class="sidebar-brand sidebar-brand-sm">
+                        <a href="index.html">MJ</a>
+                    </div>
+                    <ul class="sidebar-menu">
+                        <li class="menu-header">Dashboard</li>
+                        <li><a class="nav-link" href="blank.html"><i class="fas fa-fire"></i> <span>Dashboard</span></a>
+                        </li>
+                        <li class="menu-header">Transaksi</li>
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-shopping-cart"></i> <span>Penjualan</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="layout-default.html">Entry Penjualan</a></li>
+                                <li><a class="nav-link" href="layout-transparent.html">Daftar Penjualan</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-truck"></i> <span>Pembelian</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="layout-default.html">Entry Pembelian</a></li>
+                                <li><a class="nav-link" href="layout-transparent.html">Daftar Pembelian</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-header">Master Data</li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-box"></i>
+                                <span>Data Item</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-th"></i>
+                                <span>Data Kategori</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-square"></i>
+                                <span>Data Satuan</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-parachute-box"></i>
+                                <span>Data Supplier</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-users"></i>
+                                <span>Data Customer</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-user-check"></i>
+                                <span>Data Karyawan</span></a></li>
+                        {{-- <li><a class="nav-link" href="credits.html"><i class="fas fa-database"></i>
+                                <span>Data Service / Jasa</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-database"></i>
+                                <span>Stok Opname</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-database"></i>
+                                <span>Stok In / Out</span></a></li> --}}
+                        <li class="menu-header">Laporan</li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-copy"></i>
+                                <span>Laporan Barang</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-copy"></i>
+                                <span>Laporan Penjualan</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-copy"></i>
+                                <span>Laporan Pembelian</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-copy"></i>
+                                <span>Laporan Stok Opname</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-copy"></i>
+                                <span>Laporan Laba Rugi</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-copy"></i>
+                                <span>Laporan Kas</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-copy"></i>
+                                <span>Laporan Kas Bank</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-copy"></i>
+                                <span>Laporan Stok In / Out</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-copy"></i>
+                                <span>Laporan Hutang</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-copy"></i>
+                                <span>Laporan Piutang</span></a></li>
+                        <li class="menu-header">Management User</li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-users"></i>
+                                <span>Data User</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-history"></i>
+                                <span>User Log</span></a></li>
+                        <li class="menu-header">Grafik</li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-chart-line"></i>
+                                <span>Grafik</span></a></li>
+                        <li class="menu-header">Tools</li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-barcode"></i>
+                                <span>Generate Barcode</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-hourglass-start"></i>
+                                <span>Backup Data</span></a></li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-history"></i>
+                                <span>Application Log</span></a></li>
+                        <li class="menu-header">Prestasi</li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-trophy"></i>
+                                <span>Index Prestasi Karyawan</span></a></li>
+                    </ul>
+
+                    <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+                        <a href="#" class="btn btn-primary btn-lg btn-block btn-icon-split">
+                            <i class="fas fa-store"></i> Profile Toko
+                        </a>
+                    </div>
+
+                </aside>
+            </div>
